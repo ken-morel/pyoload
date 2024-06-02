4be038ce03b8b11468b520c988af7d502491d16a
ken-morel <engonken8@gmail.com>
2024-06-02 22:33:08 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,53 +1,57 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
-import sys
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
-
+import sys
 
 mydir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 sys.path.insert(0, mydir)
 
-sys.setrecursionlimit(500)
-project = 'pyoload'
-copyright = '2024, ken-morel'
-author = 'ken-morel'
+# -- Project information -----------------------------------------------------
 
-release = '1.1.3'
-version = '1.1.3'
+project = "dashing"
+copyright = "2020, Federico Ceratto"
+author = "Federico Ceratto"
 
-# -- General configuration
+master_doc = "index"
 
-master_doc = 'index'
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 autosummary_generate = True
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "_templates"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "furo"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# Show both the class' docstring, and `__init__` docstring:
 autoclass_content = "both"
-
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-    'github': ('https://github.com/', None),
-}
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
-
-# -- Options for HTML output
-
-html_theme = "sphinxawesome_theme"
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
-
-rst_prolog = """\
-.. role:: python(code)
-  :language: python
-
-"""
