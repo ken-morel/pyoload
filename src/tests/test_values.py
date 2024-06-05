c5fff054b530cfb32fc8589853976c5f0b48bdbe
ken-morel <engonken8@gmail.com>
2024-06-05 09:49:09 +0100

    Create test_values.py

---
+++ src/tests/test_values.py
@@ -0,0 +1,22 @@
+from pyoload import *
+import pyoload
+
+
+assert pyoload.__version__ == '2.0.0'
+
+
+@annotate
+def odd(a: Values(range(10))) -> bool:
+    return a % 2 == 1
+
+
+def test_values():
+    assert odd(3), '3 reported not odd'
+    assert not odd(2), '2 reported odd'
+
+    try:
+        odd(10)
+    except AnnotationError:
+        pass
+    else:
+        raise AssertionError('Values did not crash')
